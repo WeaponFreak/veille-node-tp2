5fda7c345f1227192001b1c6b570d28858d55465
Ricardo <ricardoquebec@live.com>
2018-03-23 16:24:50 -0400

    ajout de app.post modifier_ajax

--- 01-app.js
+++ 01-app.js
@@ -147,3 +147,16 @@ nsp.on('connection', function(socket){
 app.get('/chat', (req, res) => {
 	res.render('socket_vues.ejs')
 })
+/////
+app.post('/modifier_ajax', (req, res) => {
+	console.log('route /ajax_modifier');
+	 console.log('req.body = ' + util.inspect(req.body));
+	req.body._id = 	ObjectID(req.body._id);
+	console.log('req.body._id = ' + req.body._id);
+	db.collection('adresse').save(req.body, (err, result) => {
+		if (err) return console.log(err);
+		console.log('sauvegarder dans la BD');
+		console.log('req.body = ' + util.inspect(req.body));
+		res.send(JSON.stringify(req.body));
+	})
+})
